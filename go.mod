9de68cdf465c1a0dfdbfb2ffe6b0d214d3a1f87b
Minal Kyada <37657791+minal-kyada@users.noreply.github.com>
2024-11-15 18:05:02 -0600

    config files

---
+++ go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.23.2
+
+require (
+	github.com/kelvins/geocoder v0.0.0-20231112130812-98d82c75e49b
+	github.com/lib/pq v1.10.9
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"main/services"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+
+	// Establish connection to Postgres Database
+	// db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=localhost sslmode=disable"  //local
+	// db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=postgresdb sslmode=disable port = 5433" //docker
+
+	//Database application running on Google Cloud Platform.
+	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=cbi-gcp-tutorial-a20544029:us-central1:postgres sslmode=disable port = 5432"
+
+	db, err := sql.Open("postgres", db_connection)
+	if err != nil {
+		panic(err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+
+	// Spin in a loop and pull data from the city of chicago data portal
+	// Once every hour, day, week, etc.
+	// Though, please note that Not all datasets need to be pulled on daily basis
+	// fine-tune the following code-snippet as you see necessary
+	for {
+		// build and fine-tune functions to pull data from different data sources
+		// This is a code snippet to show you how to pull data from different data sources.
+
+		services.GetUnemploymentRates(db)
+		fmt.Println("Done with unemployement rates")
+
+		services.GetBuildingPermits(db)
+		fmt.Println("Done with building permits")
+
+		services.GetTaxiTrips(db)
+		fmt.Println("Done with taxi trips")
+
+		// Pull the data once a day
+		// You might need to pull Taxi Trips and COVID data on daily basis
+		// but not the unemployment dataset becasue its dataset doesn't change every day
+		time.Sleep(24 * time.Hour)
+	}
+
+}
